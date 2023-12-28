ff9509c503a47fdadfb476e83efec7dff84b9b8e
LM-01 <mollinedo.lester@gmail.com>
2023-12-28 16:55:02 -0500

    Updating route

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/endpoint', (req: Request, res: Response) => {
-   res.json({ message: 'This is an endpoint!' });
+   res.json({ message: 'It Works!' });
 });
 
 export default router;
